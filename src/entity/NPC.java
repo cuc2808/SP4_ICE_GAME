74ff959e0cd319ade807ae8577fbc5aee5c6eff4
jonasE-10 <jonas.ellekjaer10@gmail.com>
2025-12-08 13:21:15 +0100

    Created NPC --> fixed place on map --> added movement on X axis

--- src/entity/NPC.java
+++ src/entity/NPC.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public class NPC extends Entity {
     GamePanel gp;
+    protected int actionCounter = 0;
     protected boolean hasMainEvent;
     protected boolean hasSideEvent;
 
@@ -25,7 +26,15 @@ public class NPC extends Entity {
 
     }
     public void update(){
-
+        actionCounter++;
+        if (actionCounter < 21){
+            worldX += movementspeed;
+        } else {
+            worldX -= movementspeed;
+        }
+        if (actionCounter == 40) {
+            actionCounter = 0;
+        }
     }
     public void draw(Graphics g2){
 
@@ -36,8 +45,15 @@ public class NPC extends Entity {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        int screenX = worldX - gp.player.worldX + gp.player.screenX;
+        int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
-        g2.drawImage(playerImage, worldX, worldY, gp.tileSize, gp.tileSize, null);
+        if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX &&
+                worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+                worldY + gp.tileSize > gp.player.worldY - gp.player.screenY &&
+                worldY - gp.tileSize < gp.player.worldX + gp.player.screenX) {
+            g2.drawImage(playerImage, screenX, screenY, gp.tileSize, gp.tileSize, null);
+        }
 
 
     }
