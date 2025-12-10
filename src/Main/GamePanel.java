4fa5f1f173b2b5c78d06f05aa79394a5e7802138
Gabriel080103 <gabrielmarcussen@gmail.com>
2025-12-10 12:39:20 +0100

    Player collision size

--- src/Main/GamePanel.java
+++ src/Main/GamePanel.java
@@ -4,119 +4,194 @@ import Tile.TileManager;
 import entity.NPC;
 import util.FileIO;
 import util.SoundSystem;
+
 import entity.Player;
 
+import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferStrategy;
 
-public class GamePanel extends Canvas implements Runnable {
 
-    // ===== SCREEN SETTINGS =====
-    final int originalTileSize = 32;
-    final int scale = 3;
-    public final int tileSize = originalTileSize * scale;
 
-    public int maxScreenCollum = 9;
-    public int maxScreenRow = 9;
-    public final int screenWidth = tileSize * maxScreenCollum;
-    public final int screenHeight = tileSize * maxScreenRow;
+public class GamePanel extends JPanel implements Runnable {
+    // Cutscene variables
+    boolean isFading = false;
+    float fadeAlpha = 0f;   // 0 = fully transparent, 1 = fully black
+    float fadeSpeed = 0.02f; // Adjust for speed
+    boolean fadeOut = true;  // true = fading to black, false = fading in
+    Runnable fadeCallback;   // What to do when fade finishes
 
-    // ===== WORLD SETTINGS =====
+    //SCREEN SETTINGS - includes resolution, tile size and so on:
+    //                  ===Attributes===
+    final int originalTileSize = 32;        //We're making a 2D game and deciding on the size for each Tile: 32x32.
+    final int scale = 3;        //As 32x32 pixel isn't much, we upscale it. That's why we create an attribute for it. Here it's: x3.
+
+    public final int tileSize = originalTileSize * scale; //Therefor this attribute will be the one we use, as it takes into account the previous.
+    // ^ 96x96 tiles.
+
+    //We now have a size for our building blocks, now we decide on how many building blocks our game can contain.
+    public int maxScreenCollum = 9; //The amount of tiles * x
+    public int maxScreenRow = 9; //The amount of tiles * Y
+    public final int screenWidth = tileSize * maxScreenCollum; //Screen width. 1536 Pixels.
+    public final int screenHeight = tileSize * maxScreenRow; //Screen length. 1152 Pixels.
+
+        //WORLD SETTINGS
     public final int maxWorldCol = 30;
     public final int maxWorldRow = 30;
     public final int worldWidth = tileSize * maxWorldCol;
-    public final int worldHeight = tileSize * maxWorldRow;
+    public final int worldHieght = tileSize * maxWorldRow;
 
+    // FPS FRAMES PER SECOND:
     int FPS = 60;
 
     public CollisionChecker colCheck = new CollisionChecker(this);
+    public CutsceneManager cutsceneManager = new CutsceneManager(this);
+
+
     TileManager tileM = new TileManager(this);
     FileIO io = new FileIO(this);
     SoundSystem soundSystem = new SoundSystem(io);
-    KeyHandler keyH = new KeyHandler();
-
+    KeyHandler keyH = new KeyHandler(); //We need to instantiate the Handler to use it.
     public GUI gui = new GUI(this);
-    Thread gameThread;
-
-    public Player player = new Player(this, keyH);
+    Thread gameThread;       // This makes the game running instead of static. "A thread is a thread of execution in a program." It keeps running until the "Run" is executed. -- There is also added a method called run.
+    public Player player = new Player(this,keyH);
     public NPC npc = new NPC(this);
 
-    // ===== Constructor =====
+
+    //      ===== Constructor =====
     public GamePanel() {
-        setPreferredSize(new Dimension(screenWidth, screenHeight));
-        setBackground(Color.BLACK);
-        addKeyListener(keyH);
-        setFocusable(true);
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));        //this =  is our Class, that we then get the size with Height * Length. We also use a new command Java just imports called Dimension = (H x L).
+        this.setBackground(Color.BLACK);      //Not all needed, but we get blue background. COLOR. is goated.
+        this.setDoubleBuffered(true);       //It helps with rendering/faster load. Basically it draws the program in another window we can't see before getting displayed.
+        this.addKeyListener(keyH);      //We make sure to add the *Specifik KeyListener to the program. Here it's keyH.
+        this.setFocusable(true);        //Basically makes it so it's focused on keyInput.
     }
 
     public void startGameThread() {
-        gameThread = new Thread(this);
-        gameThread.start();
+
+        gameThread = new Thread(this); //We're here passing the "GamePanel" class to the thread. So now the thread is instantiated.
+        gameThread.start();  //This will call the 'run' method.
+
+        //soundSystem.musicBreak("Resources/soundFiles/dry-fart.wav");
         soundSystem.playTrack("Resources/musicFiles/mainTheme.wav");
     }
 
     @Override
-    public void run() {
+    public void run() {         //Runnable adds this methode as it's implemented. And our Thread automatically calls this method when we start the gameThread.
 
-        createBufferStrategy(2);   // DOUBLE BUFFER STRATEGY
-        BufferStrategy bs = getBufferStrategy();
-
-        double drawInterval = 1000000000.0 / FPS;
+        double drawInterval = 1000000000/FPS;       //We use nanoseconds to limit our Framerate. This is equal to 0.01666 which means it draws 60 times per 1 second.
         double delta = 0;
-        long lastTime = System.nanoTime();
+        long lastTime = System.nanoTime(); //Time of the method called.
+        long currentTime;
         long timer = 0;
         int drawCount = 0;
 
         while (gameThread != null) {
 
-            long currentTime = System.nanoTime();
-            delta += (currentTime - lastTime) / drawInterval;
-            timer += (currentTime - lastTime);
-            lastTime = currentTime;
+            currentTime = System.nanoTime();        //The current time of the while loop.
 
-            if (delta >= 1) {
+            delta += (currentTime - lastTime) / drawInterval;        //delta basically tells us how much time has passed.
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;         //Now we set the current time to the last timne - acts like time, so we can use it again once the loops runs again.
+
+
+            if(delta >= 1){      //Since we divide the time passed with drawInterval then we end up updating when after a certain time. Here it is 60 FPS.
+                //UPDATE: this update information.
                 update();
-                render(bs);
-                delta--;
+
+                //DRAW: this draws our graphics and information.
+                repaint(); //repaint is how you call paintComponent
+
+                delta--;        //We subtract from delta so it works when we reset or run the loop again.
                 drawCount++;
             }
 
-            if (timer >= 1000000000) {
+            if(timer >= 1000000000){     //Everytime the timer reaches so many nanoseconds, it counts a one second, and there we display the FPS or the drawCount. It should show the given FPS.
+                                         // This also works to see if we update correctly.
+                                         //It's basically an FPS counter.
                 System.out.println("FPS: " + drawCount);
-                drawCount = 0;
+                drawCount = 0;      //We also have to reset them, so they don't just count upwards for eternity.
                 timer = 0;
             }
         }
     }
 
-    public void update() {
-        player.update();
-        npc.update();
+    public void update() { //
+        if (isFading) {
+        System.out.println("fadeAlpha: " + fadeAlpha);
     }
 
-    // ===== NEW RENDER METHOD (no screen tearing!) =====
-    private void render(BufferStrategy bs) {
+        // Trigger cutscene if player is at row10 col2 (tree)
+        int playerCol = player.worldX / tileSize;
+        int playerRow = player.worldY / tileSize;
 
-        do {
-            do {
-                Graphics2D g2 = (Graphics2D) bs.getDrawGraphics();
+        if (!cutsceneManager.isActive() && playerCol == 2 && playerRow == 9) {
+            cutsceneManager.startCutscene();
+        }
 
-                // CLEAR SCREEN
-                g2.clearRect(0, 0, screenWidth, screenHeight);
+        // Only update entities if cutscene is not active
+        if (!cutsceneManager.isActive()) {
+            player.update();
+            npc.update();
+        } else {
+            cutsceneManager.update();
+        }
+        if (isFading) {
+            if (fadeOut) {
+                fadeAlpha += fadeSpeed;
+                if (fadeAlpha >= 1f) {
+                    fadeAlpha = 1f;
+                    fadeOut = false; // Next phase: fade in
+                    if (fadeCallback != null) fadeCallback.run(); // Trigger map change
+                }
+            } else {
+                fadeAlpha -= fadeSpeed;
+                if (fadeAlpha <= 0f) {
+                    fadeAlpha = 0f;
+                    isFading = false; // Fade complete
+                }
+            }
 
-                // DRAW GAME
-                tileM.draw(g2);
-                npc.draw(g2);
-                player.draw(g2);
-                gui.draw(g2);
+        }
 
-                g2.dispose();
+    }
+    public void fadeToNewMap(Runnable mapChange) {
+        isFading = true;
+        fadeAlpha = 0f;
+        fadeOut = true;
+        fadeCallback = mapChange;
+    }
 
-            } while (bs.contentsRestored());
 
-            bs.show();
-            Toolkit.getDefaultToolkit().sync(); // reduces tearing on some systems
+    public void paintComponent(Graphics g) { //This is a method by Java in the JFrame package, it draws graphics.
+        //We have to call the superClass, in this case JFrame.
+        super.paintComponent(g);
 
-        } while (bs.contentsLost());
+        Graphics2D g2 = (Graphics2D)g;      //This method has more functions
+
+        tileM.draw(g2);
+
+        //NPC
+        npc.draw(g2);
+
+
+        //It works a lot like processing... (very nice, it takes me back a whole 2 months !!!)
+        player.draw(g2);
+
+        gui.draw(g2);
+
+
+
+        if (isFading) {
+            System.out.println("test1");
+            Graphics2D g2d = (Graphics2D) g.create(); // Create a copy of g2d
+
+            g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, fadeAlpha));
+            g2d.setColor(Color.BLACK);
+            g2d.fillRect(0, 0, screenWidth, screenHeight);
+            g2d.dispose();
+            this.player.movementSpeed = 20;
+        }
+        g2.dispose();
+
     }
 }
