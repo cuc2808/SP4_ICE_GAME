28f3a8616939b6415bd7b746de73e26655675e87
jonasE-10 <jonas.ellekjaer10@gmail.com>
2025-12-11 14:45:35 +0100

    Fixed Collision, NPC

---
+++ src/object/Object.java
@@ -0,0 +1,32 @@
+package object;
+
+import Main.GamePanel;
+import util.FileIO;
+
+import javax.swing.text.html.HTMLDocument;
+import java.awt.image.BufferedImage;
+
+public class Object {
+
+    GamePanel gp;
+    FileIO io;
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int worldX,worldY;
+
+    public Object (GamePanel gp, FileIO io) {
+        this.gp = gp;
+        this.io = io;
+
+        loadImage();
+
+    }
+    public void setDefaultValues(){
+    }
+    public void loadImage(){
+        io.readImage("Resouces/Files/ObjectImages/"+name+".png");
+    }
+
+
+}
